38d9537cd26c5256b6c0be5837c29e3be51e520f
dailydm1227 <dailydm1227@gmail.com>
2020-12-15 18:07:44 -0700

    add low res and hi res images

---
+++ src/api/data.js
@@ -0,0 +1,72 @@
+export const SongJSON = () => ({
+  dyingtolive: {
+    images: [
+      {
+        height: 640,
+        width: 453,
+        url: '/img/dyingtolive/cover-hd.jpg'
+      },
+      {
+        height: 64,
+        width: 45,
+        url: '/img/dyingtolive/cover.jpg'
+      }
+    ],
+  },
+  videodays: {
+    images: [
+      {
+        height: 640,
+        width: 336,
+        url: '/img/videodays/cover-hd.jpg'
+      },
+      {
+        height: 64,
+        width: 34,
+        url: '/img/videodays/cover.jpg'
+      }
+    ],
+  },
+  welcometohell: {
+    images: [
+      {
+        height: 640,
+        width: 512,
+        url: '/img/welcometohell/cover-hd.jpg'
+      },
+      {
+        height: 64,
+        width: 51,
+        url: '/img/welcometohell/cover.jpg'
+      }
+    ],
+  },
+  baker3: {
+    images: [
+      {
+        height: 640,
+        width: 449,
+        url: '/img/baker3/cover-hd.jpg'
+      },
+      {
+        height: 64,
+        width: 45,
+        url: '/img/baker3/cover.jpg'
+      }
+    ],
+  },
+  fullyflared: {
+    images: [
+      {
+        height: 640,
+        width: 453,
+        url: '/img/fullyflared/cover-hd.jpg'
+      },
+      {
+        height: 64,
+        width: 45,
+        url: '/img/fullyflared/cover.jpg'
+      }
+    ],
+  },
+});
